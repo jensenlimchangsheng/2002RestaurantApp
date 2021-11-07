12845bdf85c9f35ed288a9f127cbd395dbb2edf2
shunyao643 <63784032+shunyao643@users.noreply.github.com>
2021-11-07 21:32:05 +0800

    Update Customer.java

--- src/project2002/Customer.java
+++ src/project2002/Customer.java
@@ -2,14 +2,32 @@ package project2002;
 
 public class Customer {
 
-	public Customer(String customername, int number) {
-		// TODO Auto-generated constructor stub
+	private String custName;
+	private int number;
+
+	public Customer(String customerName, int number) {
+		this.custName = customerName;
+		this.number = number;
+	}
+
+	public String getCustName() {
+		return this.custName;
+	}
+
+	public int getNumber() {
+		return this.number;
 	}
 
 	@Override
 	public boolean equals(Object o) {
-		// TODO create equals method for ReservationHandlers
-		return false;
+		if (o == this)
+			return true;
+
+		if (!(o instanceof Customer))
+			return false;
+
+		Customer c = (Customer) o;
+		return this.number == c.getNumber() && this.custName.equals(c.getCustName());
 	}
 
 }
