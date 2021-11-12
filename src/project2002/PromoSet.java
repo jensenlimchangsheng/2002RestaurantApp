69776e54fb43270fa1469d85c3513cc85140998c
babyjty <60604398+babyjty@users.noreply.github.com>
2021-11-12 23:22:05 +0800

    added promo get and set

--- src/project2002/PromoSet.java
+++ src/project2002/PromoSet.java
@@ -4,9 +4,18 @@ import project2002.MenuUI.ItemType;
 
 public class PromoSet extends MenuItem {
 	ItemType itemType = ItemType.PROMO;
+	String specialReq;
 
 	PromoSet(String name, double price, ItemType itemType, String description) {
 		super(name, price, itemType, description);
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getSpecialReq() {
+		return specialReq;
+	}
+
+	public void setSpecialReq(String specialReq){
+		this.specialReq = specialReq;
 	}
 }
