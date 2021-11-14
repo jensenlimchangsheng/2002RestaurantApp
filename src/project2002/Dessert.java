d2c5a550a352d458ab13a900dac82899e83f0e54
babyjty <60604398+babyjty@users.noreply.github.com>
2021-11-14 08:45:16 +0800

    get set

--- src/project2002/Dessert.java
+++ src/project2002/Dessert.java
@@ -4,8 +4,17 @@ import project2002.MenuUI.ItemType;
 
 public class Dessert extends MenuItem {
 	ItemType itemType = ItemType.DESSERT;
+	private Boolean nuts;
 
 	Dessert(String name, double price, ItemType itemType, String description) {
 		super(name, price, itemType, description);
+	}
+
+	public boolean getNuts(){
+		return nuts;
+	}
+
+	public void setNuts(boolean nuts){
+		this.nuts = nuts;
 	}
 }
--- src/project2002/Side.java
+++ src/project2002/Side.java
@@ -4,8 +4,17 @@ import project2002.MenuUI.ItemType;
 
 public class Side extends MenuItem {
 	ItemType itemType = ItemType.SIDE;
+	String size;
 
 	Side(String name, double price, ItemType itemType, String description) {
 		super(name, price, itemType, description);
+	}
+
+	public String getSize(){
+		return size;
+	}
+
+	public void setSize(String size){
+		this.size = size;
 	}
 }
